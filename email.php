5a9183b117e63612404b8bd40cc373cb3b5c3a45
emilianofunk <emiliano.fant@gmailm.com>
2016-04-23 13:54:47 -0300

    - Test mail

--- email.php
+++ email.php
@@ -19,45 +19,8 @@
 			$message = $_REQUEST['message'];
 			// Send contact information
 			$mail = mail( $to, $subject , $message, $header );
+			var_dump($mail);
 			return $mail ? "success" : "error";
 		else:
 			return "error";
 		endif;
-
-    //Status API Training Shop Blog About
-
-    /*
-
-		<div class="col-sm-12">
-		<form class="form-horizontal" role="form" id="formContacto">
-			<div class="col-sm-6">
-				<div class="form-group">
-					<div class="span-form">
-						<input class="form-control" type="text" placeholder="Nombre" id="first_name" required="required">
-						</div>
-						<div class="span-form">
-							<input type="text" class="form-control" placeholder="Apellido" id="last_name" required="required">
-						</div>
-						</div>
-						<div class="form-group">
-							<input type="email" class="form-control" id="email" placeholder="Direccion de email" required="required">
-						</div>
-						<div class="form-group">
-							<input type="text" class="form-control" id="subject" placeholder="Titulo" required="required">
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="form-group">
-							<textarea name="" id="message" class="form-control btn-block" rows="3"></textarea>
-						</div>
-						<div class="form-group">
-							<input type="submit" class="btn btn-block" value="Enviar Mensaje">
-						</div>
-					</div>
-				<div class="col-sm-12">
-				<p class="contact-success">Your Message has been Successfully Sent!</p>
-				<p class="contact-error">Error! Something went wrong!</p>
-			</div>
-		</form>
-
-    */
